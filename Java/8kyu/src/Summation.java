f33340935c8c25c2147fe0c19b8485d27c934189
hazelnutx <raulrusu46@gmail.com>
2019-10-20 15:25:53 +0300

    summation challenge kyu 8

--- Java/8kyu/src/Summation.java
+++ Java/8kyu/src/Summation.java
@@ -7,12 +7,7 @@ public class Kata {
 	}
 	
 	public static int summation(int n) {
-		int fact = 0;
-		
-		for(int i = 1; i <= n; i++) {
-			fact += i;
-		}
-		return fact;
+		return n*(n+1)/2;
 		
 	}
 	
